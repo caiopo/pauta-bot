608f5e38472eb1bb042defdcf5291a73fc822d7d
caiopo <caiopoliveira@gmail.com>
2016-10-20 21:30:08 -0200

    Initial commit :tada:

---
+++ bot.py
@@ -0,0 +1,21 @@
+import logging
+
+from os import environ
+from telegram.ext import Updater, MessageHandler, Filters
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+TOKEN = environ['TOKEN']
+APPNAME = environ['APPNAME']
+PORT = int(environ.get('PORT', '5000'))
+updater = Updater(TOKEN)
+
+def echo(bot, update):
+    return update.message.text
+
+if __name__ == '__main__':
+    updater.dispatcher.add_handler(MessageHandler([Filters.text], echo))
+
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+    updater.bot.setWebhook("https://" + APPNAME + ".herokuapp.com/" + TOKEN)
+    updater.idle()
